d2c45d78db311e62cce472a6ce6abbab1f2faf58
NoOseGG <hoc751@gmail.com>
2022-06-08 14:11:14 +0300

    added navigation, bottom navigation menu

--- app/src/main/java/com/example/kinopoisk/MainActivity.kt
+++ app/src/main/java/com/example/kinopoisk/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.kinopoisk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavHost
+import androidx.navigation.ui.setupWithNavController
+import com.example.kinopoisk.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val navController by lazy {
+        (supportFragmentManager.findFragmentById(R.id.container) as NavHost)
+            .navController
+    }
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = requireNotNull(_binding)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomMenu.setupWithNavController(navController)
     }
 }
